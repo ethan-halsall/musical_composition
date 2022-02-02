9131cdb1618552ebee885d6c29b4b251636dcba1
Ethan Halsall <ethan.halsall@gmail.com>
2022-02-02 15:26:06 +0000

    Generate blocks from each piece of music and the combine them randomly.
    - Change this random process to one involing noise
    
    Signed-off-by: Ethan Halsall <ethan.halsall@gmail.com>

--- qt.py
+++ qt.py
@@ -7,47 +7,57 @@ import sys
 import midi_helper as helper
 from l_system import lsystem, parse_lengths
 from markov import Markov
+from random import randint, choice
+import itertools
 
 
 class Worker(QRunnable):
-    def __init__(self, filename, seed=1):
+    def __init__(self, items, seed=1):
         super().__init__()
-        self.filename = filename
+        self.items = items
         self.seed = seed
 
     @pyqtSlot()
     def run(self):
-        # Extract the notes from midi file using midi helper
-        midi_extraction = helper.Extract(f"midi/{self.filename}")
-        midi_extraction.parse_midi()
-        chords = midi_extraction.get_chords()
+        blocks = []
+        for item in self.items:
+            filename = item.text()
+            # Extract the notes from midi file using midi helper
+            midi_extraction = helper.Extract(f"midi/{filename}")
+            midi_extraction.parse_midi()
+            chords = midi_extraction.get_chords()
 
-        # Generate markov chain
-        markov_chain = Markov(3)
-        markov = markov_chain.transition_matrix(chords)
+            # Generate markov chain
+            markov_chain = Markov(3)
+            markov = markov_chain.transition_matrix(chords)
 
-        # Generate a sequence of notes using the markov chain
-        success = False
-        while not success:
-            try:
-                notes = markov_chain.generate_sequence(markov)
-                success = True
-            except Exception as e:
-                print(e)
+            # Generate 50 sequence of notes using the markov chain
+            for _ in range(15):
+                success = False
+                while not success:
+                    try:
+                        # Generate a segment of length some power 2^n
+                        length = 2 ** randint(2, 3)
+                        notes = markov_chain.generate_sequence(markov, length=length)
+                        blocks.append(notes)
+                        success = True
+                    except Exception as e:
+                        print(e)
 
-        rules = {"a": "b", "b": "(a)[b]"}
-        rules = {"a": "b[a]b(a)a", "b": "bb"}
-        rules = {"a": "d[dbe](dce)e", "b": "d[daf](dcf)f", "c": "d[dbg](dag)g"}
-        #rules = {"a": "c(ba(b))c[ba[b]]", "b": "c(be)c[bf]", "c": "cgg"}
+        notes = [choice(blocks) for x in range(50)]
+
+        # rules = {"a": "b", "b": "(a)[b]"}
+        # rules = {"a": "b[a]b(a)a", "b": "bb"}
+        # rules = {"a": "d[dbe](dce)e", "b": "d[daf](dcf)f", "c": "d[dbg](dag)g"}
+        rules = {"a": "c(ba(b))c[ba[b]]", "b": "c(be)c[bf]", "c": "cgg"}
         #rules = {"a": "b[a[ba]]", "b": "b((b)a)c", "c": "cd"}
 
         tree = lsystem("a", rules, 8)
-
         durations = parse_lengths(tree)
-
-        print(durations)
-
-        helper.write_to_midi(f"{self.filename[:-4]}_{self.seed}.mid", notes, durations)
+        # print(durations)
+        self.filename = "mozasasaassaas"
+        helper.write_to_midi(f"{self.filename[:-4]}_{self.seed}.mid", list(itertools.chain.from_iterable(notes)),
+                             durations)
 
         # Play midi file using timidity binary
         os.system(f"timidity -Os out/{self.filename[:-4]}_{self.seed}.mid")
@@ -69,6 +79,8 @@ class Window(QWidget):
         for count, file in enumerate(files):
             self.list_widget.insertItem(count, file)
 
+        self.list_widget.setSelectionMode(QListWidget.ExtendedSelection)
+
         # self.listwidget.clicked.connect(self.clicked)
         layout.addWidget(self.list_widget)
 
@@ -78,13 +90,14 @@ class Window(QWidget):
         layout.addWidget(self.button_generate)
 
     def clicked(self, qmodelindex):
-        item = self.list_widget.currentItem()
-        print(item.text())
+        items = self.list_widget.currentItems()
+        print(items.text())
 
     def generate(self):
         if self.threadpool.activeThreadCount() == 0:
-            item = self.list_widget.currentItem()
-            worker = Worker(item.text())
+            items = self.list_widget.selectedItems()
+            print(items)
+            worker = Worker(items)
             self.threadpool.start(worker)
         else:
             print("Worker already running")
